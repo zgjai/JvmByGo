b97985eb90b4283057bc94bb932e8abaf7f21a44
zgjai <18710057269@163.com>
2016-11-24 09:57:56 +0800

    ch01

--- JVM/src/ch01/main.go
+++ JVM/src/ch01/main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+func startJVM(cmd *Cmd) {
+	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+}
+
 func main() {
-	fmt.Printf("hello, world\n")
-	fmt.Printf("first")
-	fmt.Printf("hello, world\n")
+	cmd := ParseCmd()
+	if cmd.versionFlag {
+		fmt.Printf("version is 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "" {
+		PrintUsage()
+	} else {
+		startJVM(cmd)
+	}
 }
